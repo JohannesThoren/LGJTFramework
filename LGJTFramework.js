ac540ee4b200bb8681ecfcd830c500b075de89a9
Johannes Thorén <johannes@lgjt.xyz>
2024-01-01 15:15:36 +0100

    Added a router
    - added a router that uses Hashes to route to different "pages", this means that we can not use 'href="#<id>"' to jump to places on the page.
    
    todo: make 'href="#<id>"' usable again.

--- LGJTFramework.js
+++ LGJTFramework.js
@@ -1,118 +1,191 @@
+/**
+ * 
+ * @param {*} name 
+ * @param  {...any} children 
+ * @returns 
+ */
 function Tag(name, ...children) {
-    let tag = document.createElement(name);
-  
-    tag.OnAppend = (call = function() {return}) => { call(); return tag; }
-  
-    tag.Arg = (name, value) => {
-      tag.setAttribute(name, value);
-      return tag
-    }
-  
-    tag.Id = (name) => {
-      tag.Arg('id', name);
-      return tag;
-    }
-  
-    tag.Class = (name) => {
-      tag.Arg('class', name);
-      return tag;
-    }
-  
-    tag.Child = (child) => {
-      if(child.OnAppend) {
-        child.OnAppend()
-      }
-      tag.appendChild(child);
-  
-      return tag;
-    }
-  
-    children.forEach(child => tag.Child(child));
-  
+  let tag = document.createElement(name);
+
+  tag.OnAppend = (call = function () { return }) => { call(); return tag; }
+
+
+  tag.Arg = (name, value) => {
+    tag.setAttribute(name, value);
     return tag
   }
-  
-  function Text(text) {
-    let textNode = document.createTextNode(text);
-    return textNode;
+
+  tag.Id = (name) => {
+    tag.Arg('id', name);
+    return tag;
   }
-  
-  function Div(...children) {
-    return Tag('div', ...children);
+
+  tag.Class = (name) => {
+    tag.Arg('class', name);
+    return tag;
   }
-  
-  function H(text, level = 1) {
-    return Tag('h' + level, Text(text));
+
+  tag.Child = (child) => {
+    if (child.OnAppend) {
+      child.OnAppend()
+    }
+    tag.appendChild(child);
+
+    return tag;
   }
-  
-  function P(text) {
-    return Tag('p', Text(text));
+
+  children.forEach(child => tag.Child(child));
+
+  return tag
+}
+
+function Text(text) {
+  let textNode = document.createTextNode(text);
+  return textNode;
+}
+
+function Div(...children) {
+  return Tag('div', ...children);
+}
+
+function H(text, level = 1) {
+  return Tag('h' + level, Text(text));
+}
+
+function P(text) {
+  return Tag('p', Text(text));
+}
+
+function A(text, link) {
+  return Tag('a', Text(text)).Arg("href", link)
+}
+
+function Li(...children) {
+  return Tag('li', ...children);
+}
+
+function Ol(...children) {
+  return Tag('ol', ...children);
+}
+
+function Ul(...children) {
+  return Tag('ul', ...children);
+}
+
+function Img(src, alt) {
+  return Tag('img').Arg("src", src).Arg("alt", alt)
+}
+
+function Button(text, click) {
+  let button = Tag('button').Child(Text(text));
+  button.addEventListener('click', click);
+  return button;
+}
+
+/**
+ * 
+ * @param {*} name 
+ * @param {*} body 
+ * @returns 
+ */
+function Tab(name, body) {
+  return { name: name, body: body }
+}
+
+/**
+ * 
+ * @param  {...any} tabs 
+ * @returns 
+ */
+function TabRoot(...tabs) {
+  let tabNav = Div().Id("tab-nav");
+  let tabBody = Div().Id("tab-body");
+  let tabRoot = Div().Id("tab-root");;
+
+
+  tabs.forEach(tab => {
+    let btn = Button(tab.name, () => {
+      tabBody.replaceChildren(tab.body);
+      tabRoot.Active.button.Arg("data-active", false)
+      tabRoot.Active = { button: btn, body: tab.body, name: tab.name }
+      tabRoot.Active.button.Arg("data-active", true)
+
+    }).Class("tab-nav-item").Arg("data-tab", tab.name).Arg("data-active", false);
+
+    tabNav.Child(btn)
+
+  })
+
+  tabRoot.Active = { button: tabNav.children[0], body: tabs[0].body, name: tabs[0].name };
+  tabBody.replaceChildren(tabRoot.Active.body);
+  tabRoot.Active.button.Arg("data-active", true)
+
+
+  return tabRoot.Child(tabNav).Child(tabBody);
+}
+
+/**
+ * 
+ * @param {*} path 
+ * @param {*} body 
+ * @returns 
+ */
+
+function RouterLink(text, route) {
+  return A(text, "#" + route)
+}
+
+function Route(path, body, ...subRoutes) {
+  return {
+    path: path,
+    body: body,
+    subRoutes: subRoutes,
   }
+}
+
+function RouterHandleHashChange(routingTable) {
+  let currentRoute = window.location.hash.split("#")[1] == undefined ? "/" : window.location.hash.split("#")[1]
   
-  function A(text, link) {
-    return Tag('a', Text(text)).Arg("href", link)
-  }
-  
-  function Li(...children) {
-    return Tag('li', ...children);
-  }
-  
-  function Ol(...children) {
-    return Tag('ol', ...children);
-  }
-  
-  function Ul(...children) {
-    return Tag('ul', ...children);
-  }
-  
-  function Img(src, alt) {
-    return Tag('img').Arg("src", src).Arg("alt", alt)
-  }
-  
-  function Button(text, click) {
-    let button = Tag('button').Child(Text(text));
-    button.addEventListener('click', click);
-    return button;
-  }
-  
-  function Tab(name, body) {
-    return { name: name, body: body }
-  }
-  
-  
-  // TODO: make this be dependent on queries, so that we can get urls to each tab.
-  function TabRoot(...tabs) {
-    let tabNav = Div().Id("tabNav");
-    let tabBody = Div().Id("tabBody");
-    let tabRoot = Div().Id("tabRoot");;
-  
-  
-    tabs.forEach(tab => {
-      let btn = Button(tab.name, () => {
-        tabBody.replaceChildren(tab.body);
-        tabRoot.Active.button.Arg("data-active", false)
-        tabRoot.Active = { button: btn, body: tab.body, name: tab.name }
-        tabRoot.Active.button.Arg("data-active", true)
-  
-      }).Class("tab-nav-item").Arg("data-tab", tab.name).Arg("data-active", false);
-  
-      tabNav.Child(btn)
-  
+  if (currentRoute.startsWith("/"))
+    document.querySelector("#router-body").replaceChildren(routingTable[currentRoute])
+}
+
+function Router(...routes) {
+
+  let routingTable = {}
+
+  routes.forEach(r => {
+    routingTable[r.path] = r.body
+    if (r.subRoutes.length == 0)
+      return;
+
+    r.subRoutes.forEach(sr => {
+      routingTable[r.path + sr.path] = sr.body
     })
-  
-    tabRoot.Active = { button: tabNav.children[0], body: tabs[0].body, name: tabs[0].name };
-    tabBody.replaceChildren(tabRoot.Active.body);
-    tabRoot.Active.button.Arg("data-active", true)
-  
-  
-    return tabRoot.Child(tabNav).Child(tabBody);
-  }
-  
-  
-  function OnLoad(...child) {
-    window.addEventListener('load', () => {
-      child.forEach(c => {
-        document.querySelector("body").appendChild(c);
-      })
+  })
+
+  window.addEventListener("hashchange", (evt) => RouterHandleHashChange(routingTable))
+
+  return routingTable
+}
+
+function RotuerBody() {
+  return Div().Id("router-body")
+}
+
+function RouterOnLoad(router) {
+  window.addEventListener('load', () => {
+    document.querySelector("body").appendChild(RotuerBody());
+    RouterHandleHashChange(router)
+    console.log(router)
+  })
+}
+
+function OnLoad(...child) {
+  window.addEventListener('load', () => {
+    child.forEach(c => {
+      document.querySelector("body").appendChild(c);
     })
-  }
+  })
+}
+
